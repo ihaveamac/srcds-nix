751708e51adfe9c18a6e659de37c74a1f5c1932e
ihaveahax <ian@ianburgwin.net>
2025-02-13 16:51:15 -0600

    add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,44 @@
 {
+  description = "Manage and run Source Dedicated Server games on NixOS";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, treefmt-nix }:
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      treefmtEval = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        treefmt-nix.lib.evalModule pkgs ./treefmt.nix
+      );
+    in
     {
       nixosModules = rec {
         srcds = (import ./modules).srcds;
         default = srcds;
       };
+
       nixosConfigurations.container = nixpkgs.lib.nixosSystem {
         modules = [ ./configuration.nix ];
       };
+
+      formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
+      checks = forAllSystems (system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      });
     };
 }
